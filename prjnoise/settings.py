a18ccb6d3d46ee7e1bb335399f376bca574bd105
efahmad <s.ahmad.ef@gmail.com>
2016-06-10 20:00:59 +0430

    settings

--- prjnoise/settings.py
+++ prjnoise/settings.py
@@ -136,7 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'appnoise/static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'appnoise/static'),
-)
+STATICFILES_DIRS = ()
 
